ad347dea07aee1981653c3d406278c51fd4ad86e
Davi Mello <davimello1994@gmail.com>
2021-02-17 17:53:49 -0500

    debugging for react-code-blocks

--- src/Home.jsx
+++ src/Home.jsx
@@ -84,7 +84,7 @@ export const Home = () => {
         text={recursive}
         language="javascript"
         codeBlock
-        theme={toggleCodeTheme}
+        theme={github}
         showLineNumbers={false}
       />
 
